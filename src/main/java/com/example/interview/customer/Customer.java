a0d19072a518c71f54e724f073b4f086bbd7ceb8
jonathanbalbs <jonathanesh50@gmail.com>
2023-10-30 14:32:57 +0300

    created customer

---
+++ src/main/java/com/example/interview/customer/Customer.java
@@ -0,0 +1,67 @@
+package com.example.interview.customer;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "customer")
+public class Customer implements UserDetails {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String customerId;
+    private String name;
+    private String email;
+    private String pin;
+
+    @Enumerated
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return pin;
+    }
+
+    @Override
+    public String getUsername() {
+        return customerId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
